6692cf20ac8286d1890ff5883a06a27227238163
alg0r1tm <alg0r1thm@pm.me>
2019-01-04 12:33:49 +0200

    Add PriceListContext to DataBaseLibrary

---
+++ DataBaseLibrary/Context/PriceListContext.cs
@@ -0,0 +1,22 @@
+﻿using SQLite.CodeFirst;
+using System.Data.Entity;
+
+namespace DataBaseLibrary.Context
+{
+    class PriceListContext:DbContext
+    {
+        //public virtual DbSet<User> Users { get; set; }
+
+        public PriceListContext()
+        : base("default")
+        { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<PriceListContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+
+        }
+
+    }
+}
