2c5979908f8838fa968e0a18fb493a30f869c903
alg0r1tm <alg0r1thm@pm.me>
2019-01-04 13:35:16 +0200

    Add Models to PriceListContext

--- DataBaseLibrary/Context/PriceListContext.cs
+++ DataBaseLibrary/Context/PriceListContext.cs
@@ -1,11 +1,16 @@
-﻿using SQLite.CodeFirst;
+﻿using DataBaseLibrary.Models;
+using SQLite.CodeFirst;
 using System.Data.Entity;
 
 namespace DataBaseLibrary.Context
 {
     class PriceListContext:DbContext
     {
-        //public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Furnizor> Furnizors { get; set; }
+        public virtual DbSet<Image> Images { get; set; }
+        public virtual DbSet<Price> Prices { get; set; }
+        public virtual DbSet<Resource> Resources { get; set; }
+        public virtual DbSet<Units> Unitss { get; set; }
 
         public PriceListContext()
         : base("default")
--- PriceWindowsForm/Form1.cs
+++ PriceWindowsForm/Form1.cs
@@ -16,5 +16,10 @@ namespace PriceWindowsForm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
